1289c0d1308e37b6e2c2ee538ae2e32b43939a1e
Timur <tima.bikmetov06@mail.ru>
2026-06-16 16:37:35 +0300

    feat: add refuel table

---
+++ backend/src/main/resources/db/migrations/V6__create_refuel_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS refuel
+(
+    id UUID PRIMARY KEY,
+    liters NUMERIC(10, 2) NOT NULL CHECK ( liters > 0 ),
+    cost NUMERIC(10,2) NOT NULL CHECK (cost >= 0),
+
+    fuel_type VARCHAR(32) NOT NULL,
+    fuel_name VARCHAR(32) NOT NULL,
+    station_name VARCHAR(255) NOT NULL,
+
+    CONSTRAINT fk_refuel_event
+    FOREIGN KEY (id)
+    REFERENCES timeline_events(id)
+)
