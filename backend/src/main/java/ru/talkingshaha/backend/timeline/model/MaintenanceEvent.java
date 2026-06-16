6a0137573a7bee6a6d2546339ac1ef63d0441d51
Timur <tima.bikmetov06@mail.ru>
2026-06-16 18:57:29 +0300

    feat: add Maintenance entity

---
+++ backend/src/main/java/ru/talkingshaha/backend/timeline/model/MaintenanceEvent.java
@@ -0,0 +1,24 @@
+package ru.talkingshaha.backend.timeline.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "maintenance")
+public class Maintenance extends BaseEvent {
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(name = "mileage_km", nullable = false)
+    private Integer mileageKm;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal cost;
+}
